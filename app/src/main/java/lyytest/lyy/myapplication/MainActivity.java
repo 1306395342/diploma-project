9cc4a171b43fc699e11fbfa43a8455be25275343
Sampson <1306395342@qq.com>
2019-12-31 16:47:29 +0800

    3

--- app/src/main/java/lyytest/lyy/myapplication/MainActivity.java
+++ app/src/main/java/lyytest/lyy/myapplication/MainActivity.java
@@ -4,12 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.v("lyy","lyy");
-        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.userindex);
     }
 }
